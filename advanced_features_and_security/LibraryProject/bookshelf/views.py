ad01249a190ce3a5b2884c3223e49b6cdd57f0a1
Brian-Samson01 <brian.s.mwangi01@gmail.com>
2026-02-23 20:48:26 +0300

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
 # Create your views here.
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def view_books(request):
+    return HttpResponse("You can view books")
+
+
+@permission_required("bookshelf.can_create", raise_exception=True)
+def create_book(request):
+    return HttpResponse("You can create a book")
+
+
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def edit_book(request):
+    return HttpResponse("You can edit a book")
+
+
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete_book(request):
+    return HttpResponse("You can delete a book")
+
+"""
+Permissions and Groups Setup:
+
+Groups:
+- Viewers: can_view
+- Editors: can_view, can_create, can_edit
+- Admins: can_view, can_create, can_edit, can_delete
+
+Permissions are defined in Book model Meta class
+and enforced using @permission_required decorators.
+"""
