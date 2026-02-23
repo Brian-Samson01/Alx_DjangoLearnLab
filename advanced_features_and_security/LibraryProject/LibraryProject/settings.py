004c3a69495822038b3c1e55435c85de1f359b1c
Brian-Samson01 <brian.s.mwangi01@gmail.com>
2026-02-23 15:42:03 +0300

    Implementing a Custom User Model in Django update

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -121,4 +121,4 @@ STATIC_URL = 'static/'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'list_books'
 LOGOUT_REDIRECT_URL = 'login'
-AUTH_USER_MODEL = "relationship_app.CustomUser"
+AUTH_USER_MODEL = "bookshelf.CustomUser"
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,22 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import Author, Book, Library, Librarian, UserProfile
 # Register your models here.
 
-
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional Information", {
-            "fields": ("date_of_birth", "profile_photo"),
-        }),
-    )
-
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Additional Information", {
-            "fields": ("date_of_birth", "profile_photo"),
-        }),
-    )
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
+admin.site.register(UserProfile)
